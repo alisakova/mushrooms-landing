48b84bde266dd861f4274f0e01714930c0bec597
Lisakova Anastasiia <a.lisakova@osla.ru>
2018-06-19 11:42:35 +0300

    desktop ready

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.advantages-slider').slick({
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    speed: 500
+  });
+  $('.customers-slider').slick({
+    infinite: true,
+    dots: false,
+    // autoplay: true,
+    speed: 500
+  });
+});
